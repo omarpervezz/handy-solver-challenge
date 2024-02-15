c299c4db10edd366187a61619eb7c10c7afbe1a0
Omar Pervez <freelancertusar5182@gmail.com>
2024-02-15 13:29:15 +0600

    updated version

--- assets/resources/js/mobile-menu.js
+++ assets/resources/js/mobile-menu.js
@@ -1,6 +1,7 @@
 const humburger = document.getElementById('humburger')
 humburger.addEventListener('click', () => {
-  hideMobieMenu.classList.add('opened')
+  hideMobieMenu.classList.add('opened');
+  document.documentElement.classList.add('pushbar_locked');
 })
 
 const hideMobieMenu = document.querySelector('.mobile-menu-wrapper')
@@ -13,8 +14,9 @@ mobileMenuItem.forEach((e) => {
  
 })
 
-function hideMenu(element){
-if(element.classList.contains('opened')){
-element.classList.remove('opened');
-}
+function hideMenu(element) {
+  if (element.classList.contains('opened')) {
+    element.classList.remove('opened');
+    document.documentElement.classList.remove('pushbar_locked');
+  }
 }
